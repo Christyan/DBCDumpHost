6ea4ef9c77fbe489c162482a52831d62bceb9e80
Chris <chris@tauri.hu>
2024-02-01 08:28:32 +0100

    Drop CASC support from API, we will use CASCToolHost

--- ToolsAPI/SettingsManager.cs
+++ ToolsAPI/SettingsManager.cs
@@ -1,4 +1,5 @@
 ﻿using System.IO;
+using System.Reflection;
 using Microsoft.Extensions.Configuration;
 
 namespace ToolsAPI
@@ -9,7 +10,6 @@ namespace ToolsAPI
         public static string dbcDir;
         public static string cascToolHost;
         public static string siteHost;
-        public static string cacheDir;
         public static string connectionString;
         public static string apiKey;
 
@@ -20,12 +20,11 @@ namespace ToolsAPI
 
         public static void LoadSettings()
         {
-            var config = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("config.json", optional: false, reloadOnChange: false).Build();
+            var config = new ConfigurationBuilder().SetBasePath(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location)).AddJsonFile("config.json", optional: false, reloadOnChange: false).Build();
             definitionDir = config.GetSection("config")["definitionsdir"];
             dbcDir = config.GetSection("config")["dbcdir"];
             cascToolHost = config.GetSection("config")["casctoolhost"];
             siteHost = config.GetSection("config")["sitehost"];
-            cacheDir = config.GetSection("config")["cachedir"];
             connectionString = config.GetSection("config")["connectionstring"];
             apiKey = config.GetSection("config")["apikey"];
         }
